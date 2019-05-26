f4059ba2908ae49c97df9dc260fd113c96f1c33b
Joséphine Godard <josephine.godard@gmail.com>
2019-05-26 21:27:11 +0200

    MAJ + supprimer challenge

--- src/main/java/com/wildcodeschool/myProjectWithDB/controllers/SchoolController.java
+++ src/main/java/com/wildcodeschool/myProjectWithDB/controllers/SchoolController.java
@@ -3,34 +3,59 @@ package com.wildcodeschool.myProjectWithDB.controllers;
 import com.wildcodeschool.myProjectWithDB.entities.School;
 import com.wildcodeschool.myProjectWithDB.repositories.SchoolRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
+@ResponseBody
 public class SchoolController {
 
     @GetMapping("/api/schools")
-    public List<School> getSchools(@RequestParam(defaultValue = "%") String school) {
-        return SchoolRepository.selectByCountry(school);
+    public List<School> getSchool(@RequestParam(defaultValue = "%") String family) {
+        return SchoolRepository.selectByName(family);
     }
 
     @PostMapping("/api/schools")
     @ResponseStatus(HttpStatus.CREATED)
-    public School store (
+    public School store(
             @RequestParam String name,
             @RequestParam int capacity,
             @RequestParam String country
 
     ) {
-        int idGeneratedByInsertionSchool = SchoolRepository.insertIntoSchool(
+        int idGeneratedByInsertion = SchoolRepository.insert(
                 name,
                 capacity,
                 country
+
         );
-        return SchoolRepository.selectByIdSchool(
-                idGeneratedByInsertionSchool
+        return SchoolRepository.selectById(idGeneratedByInsertion);
+    }
+
+    @PutMapping("/api/schools/{id}")
+    public School update(
+            @PathVariable int id,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer capacity,
+            @RequestParam(required = false) String country
+
+    ) {
+        School school = SchoolRepository.selectById(id);
+        SchoolRepository.update(
+                id,
+                name != null ? name : school.getName(),
+                capacity != null ? capacity : school.getCapacity(),
+                country != null ? country : school.getCountry()
+
         );
+        return SchoolRepository.selectById(id);
+    }
+
+    @DeleteMapping("/api/schools/{id}")
+    public void delete(@PathVariable int id) {
+        SchoolRepository.delete(id);
+
     }
 }
-
--- src/main/java/com/wildcodeschool/myProjectWithDB/repositories/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/myProjectWithDB/repositories/SchoolRepository.java
@@ -2,7 +2,6 @@ package com.wildcodeschool.myProjectWithDB.repositories;
 
 import com.wildcodeschool.myProjectWithDB.entities.School;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.*;
@@ -16,42 +15,43 @@ public class SchoolRepository {
     private final static String DB_USER = "root";
     private final static String DB_PASSWORD = "password";
 
-    public static List<School> selectByCountry(String country) {
-        try(
+    public static List<School> selectByName(String name) {
+        try (
                 Connection connection = DriverManager.getConnection(
                         DB_URL, DB_USER, DB_PASSWORD
                 );
                 PreparedStatement statement = connection.prepareStatement(
-                        "SELECT * FROM school WHERE country LIKE ?"
+                        "SELECT * FROM school WHERE name LIKE ?"
                 );
         ) {
-            statement.setString(1, country);
+            statement.setString(1, name);
 
-            try(
+            try (
                     ResultSet resulSet = statement.executeQuery();
             ) {
-                List<School> schools = new ArrayList<>();
+                List<School> schools = new ArrayList<School>();
 
-                while(resulSet.next()){
+                while (resulSet.next()) {
                     int id = resulSet.getInt("id");
-                    String name = resulSet.getString("name");
+                    name = resulSet.getString("name");
                     int capacity = resulSet.getInt("capacity");
-                    country = resulSet.getString("country");
+                    String country = resulSet.getString("country");
+
 
                     schools.add(new School(id, name, capacity, country));
                 }
 
                 return schools;
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new ResponseStatusException(
                     HttpStatus.INTERNAL_SERVER_ERROR, "", e
             );
         }
     }
-    public static School selectByIdSchool(int id) {
-        try(
+
+    public static School selectById(int id) {
+        try (
                 Connection connection = DriverManager.getConnection(
                         DB_URL, DB_USER, DB_PASSWORD
                 );
@@ -61,60 +61,86 @@ public class SchoolRepository {
         ) {
             statement.setInt(1, id);
 
-            try(
+            try (
                     ResultSet resulSet = statement.executeQuery();
             ) {
-                if(resulSet.next()){
-
+                if (resulSet.next()) {
                     String name = resulSet.getString("name");
                     int capacity = resulSet.getInt("capacity");
                     String country = resulSet.getString("country");
 
                     return new School(id, name, capacity, country);
-                }
-                else {
+                } else {
                     throw new ResponseStatusException(
                             HttpStatus.INTERNAL_SERVER_ERROR, "unknown id in table school"
                     );
                 }
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new ResponseStatusException(
                     HttpStatus.INTERNAL_SERVER_ERROR, "", e
             );
         }
     }
 
-    public static  int insertIntoSchool(
+    public static int update(
+            int id,
             String name,
             int capacity,
             String country
+
     ) {
-        try(
+        try (
                 Connection connection = DriverManager.getConnection(
                         DB_URL, DB_USER, DB_PASSWORD
                 );
                 PreparedStatement statement = connection.prepareStatement(
-                        "INSERT INTO school (name, capacity, country) VALUES (?, ?, ?) ",
-                        Statement.RETURN_GENERATED_KEYS
+                        "UPDATE school SET name=?, capacity=?, country=? WHERE id=?"
+                );
+        ) {
+            statement.setString(1, name);
+            statement.setInt(2, capacity);
+            statement.setString(3, country);
+            statement.setInt(4, id);
 
+            return statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "", e
+            );
+        }
+    }
+
+
+    public static int insert(
+            String name,
+            Integer capacity,
+            String country
+    ) {
+        try (
+                Connection connection = DriverManager.getConnection(
+                        DB_URL, DB_USER, DB_PASSWORD
+                );
+                PreparedStatement statement = connection.prepareStatement(
+                        "INSERT INTO School (name, capacity, country) VALUES (?, ?, ?)",
+                        Statement.RETURN_GENERATED_KEYS
                 );
         ) {
             statement.setString(1, name);
             statement.setInt(2, capacity);
             statement.setString(3, country);
 
-            if(statement.executeUpdate() != 1) {
+
+            if (statement.executeUpdate() != 1) {
                 throw new ResponseStatusException(
                         HttpStatus.INTERNAL_SERVER_ERROR, "failed to insert data"
                 );
             }
 
-            try(
+            try (
                     ResultSet generatedKeys = statement.getGeneratedKeys();
             ) {
-                if(generatedKeys.next()) {
+                if (generatedKeys.next()) {
                     return generatedKeys.getInt(1);
                 } else {
                     throw new ResponseStatusException(
@@ -122,11 +148,30 @@ public class SchoolRepository {
                     );
                 }
             }
+        } catch (SQLException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "", e
+            );
         }
-        catch (SQLException e) {
+    }
+
+    public static int delete(int id) {
+        try (
+                Connection connection = DriverManager.getConnection(
+                        DB_URL, DB_USER, DB_PASSWORD
+                );
+                PreparedStatement statement = connection.prepareStatement(
+                        "DELETE FROM school WHERE id=?"
+                );
+        ) {
+            statement.setInt(1, id);
+
+            return statement.executeUpdate();
+        } catch (SQLException e) {
             throw new ResponseStatusException(
                     HttpStatus.INTERNAL_SERVER_ERROR, "", e
             );
         }
     }
 }
+
